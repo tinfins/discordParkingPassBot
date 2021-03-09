e40f802eec34995571785c47dadec415305fe75e
Taylor Clemons <22042685+tinfins@users.noreply.github.com>
2021-03-09 01:03:03 -0500

    added print statements for t/s

--- src/utils/DatabaseHelper.py
+++ src/utils/DatabaseHelper.py
@@ -97,7 +97,7 @@ class DatabaseHelper:
         task = (int(pass_num),)
         cur = self.sqliteH.execute_select(conn, sql, task)
         for row in cur:
-            print(row)
+            print(row[0])
             return bool(row == 1)
     
     def select_passes(self, conn):
